b68e8c1bb4217fed6e3cf743f7481a801cdf5b4d
Carlos Eduardo <carloseduardokadu2004@gmail.com>
2023-11-07 09:56:57 -0300

    ok trying to pullrequest

--- hooks/hooks-react/src/App.jsx
+++ hooks/hooks-react/src/App.jsx
@@ -6,29 +6,32 @@ import Home from './pages/Home'
 import About from './pages/About'
 
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
-
+import { HookUseContext } from './components/HookUseContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>React Hooks</h1>
-    <BrowserRouter>
-    <ul>
-      <li> <Link to='/' >Home</Link>
-      </li>
+    <HookUseContext> 
+      <h1>React Hooks</h1>
+      <BrowserRouter>
+      <ul>
+        <li> <Link to='/' >Home</Link>
+        </li>
 
-      <li> <Link to='/about' >Sobre</Link>
-      </li>
-    </ul>
-    
-    <Routes>
-      <Route path='/'  element={<Home/>}/> 
-      <Route path='/about'  element={<About/>}/> 
-    
-    </Routes>
-    </BrowserRouter>
+        <li> <Link to='/about' >Sobre</Link>
+        </li>
+      </ul>
+      
+      <Routes>
+        <Route path='/'  element={<Home/>}/> 
+        <Route path='/about'  element={<About/>}/> 
+      
+      </Routes>
+      </BrowserRouter>
+
+    </HookUseContext>
   
     </>
   )
