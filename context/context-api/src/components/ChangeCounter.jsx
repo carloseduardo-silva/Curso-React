5285478cc5e7d88feed8f515ee02f322d861ea98
Carlos Eduardo <carloseduardokadu2004@gmail.com>
2023-11-23 12:22:06 -0300

    ok

--- context/context-api/src/components/ChangeCounter.jsx
+++ context/context-api/src/components/ChangeCounter.jsx
@@ -4,10 +4,25 @@ const ChangeCounter = () => {
 
     const {counter, setCounter} = useCounterContext()
 
+    const handleForm = (e) =>{
+      e.preventDefault()
+      console.log('aq')
+      setCounter(counter+1)
+    }
+
 
   return (
     <div>
-        <button onClick={() => { setCounter(counter+1)}}> Change Value of Counter</button>
+      <h2>{counter}</h2>
+        <form onSubmit={handleForm}>
+
+          <label>
+            <p> Config</p>
+            <input type="text" />
+          </label>
+        <button > Change Value of Counter</button>
+        </form>
+
       
     </div>
   )
--- context/context-api/src/context/CounterContext.jsx
+++ context/context-api/src/context/CounterContext.jsx
@@ -7,7 +7,7 @@ export const CounterContext = createContext()
 //2 - ConfigProvider and datas(states + childrenProps) that will be passed
 export const CounterContextProvider = (({children}) =>{
 
-    const [counter, setCounter] = useState(3);
+    const [counter, setCounter] = useState('');
 
     return(
         <CounterContext.Provider value={{counter, setCounter}}>
